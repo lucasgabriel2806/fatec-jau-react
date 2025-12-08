97957fa050a148dcf560fac3d4de307f27d3ab0c
Lucas Gabriel de Paula Pinto <lucas.gabriellgpp@gmail.com>
2025-12-08 10:00:29 -0300

    Adicionar Programs e Title

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import Navbar from './Components/Navbar/Navbar';
 import Hero from './Components/Hero/Hero';
+import Title from './Components/Title/Title';
+import Programs from './Components/Programs/Programs';
 
 const App = () => {
   return (
     <div>
       <Navbar/>
       <Hero />
+      <div className="container">
+        <Title subTitle='Our PROGRAM' title='What We Offer' />
+        <Programs />
+      </div>
     </div>
   )
 }
